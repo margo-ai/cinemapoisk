cc06889d2e4443b56f7e9c20e7fb250803ef1d53
Margo <plokhova.margarita@yandex.ru>
2024-01-25 19:52:18 +0300

    initialized project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  type a = string;
+  const b: a = 'aaa';
+
+  console.log(b);
+  return <div className="App">Hello</div>;
+}
+
+export default App;
