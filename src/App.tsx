0017495528aab412f79cfdc1cedd39947145db2f
Margo <plokhova.margarita@yandex.ru>
2024-06-13 19:32:16 +0300

    initialized project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import { Container } from "./components/ui/Container";
+import { Header } from "./components/Header";
+
+export const App = () => {
+  useEffect(() => {}, []);
+
+  return (
+    <Container>
+      <Header>Cinemapoisk</Header>
+    </Container>
+  );
+};
